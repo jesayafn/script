c90e043ac54f9f5cc14607234a756ddf53972bce
Jesaya Fernando Napitupulu <jesayafernando9@gmail.com>
2022-06-21 12:13:42 +0700

    first commit

---
+++ install-kubectl-microk8s-v1.24-helm.sh
@@ -0,0 +1,18 @@
+#Install Microk8s (Need Snapd)
+snap install microk8s --classic --channel=1.24/stable
+
+#Install kubectl
+apt update
+apt install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y kubectl=1.24.0-00
+kubectl completion bash | tee /etc/bash_completion.d/kubectl > /dev/null
+
+#Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt update
+apt install helm
+helm completion bash > /etc/bash_completion.d/helm
